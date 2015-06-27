3729f7f52d188e157cd229e199da9d3d5c86938b
Vikram Sreedhar <vikramsreedhar@Vikrams-MacBook-Pro-3.local>
2015-06-27 13:45:34 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function(window, document, undefined) {
+  
+  var $username-container = $("#user");
+  var $title = $("#title");
+  var $text = $("#text");
+
+  //when button is clicked
+  $("button").click(function() {
+
+  });
+
+})(this, this.document);
