145cc6be0708d732eff81beae7ea7f63d714b308
Zachary Herbst <zack@Zacharys-MacBook-Pro-2.local>
2015-06-27 22:36:47 -0700

    badass

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 (function(window, document, undefined) {
 
+   Parse.initialize("PsO5rkxMEaRvzzig7IKiQgfpQVRNdQFpPYO9Ipg0", "y5TnJxnaphDQ51Ezzq5mXd8nCqRQ3MQdTUxIMLRm");   
+    
+
   var $usernameContainer = $("#user");
   var $title = $("#title");
   var $text = $("#text");
+  var $email = $("#email");
+  // $email = "tylerweitzman@gmail.com";
 
     // Set an event listener on the Choose File field.
     $('#fileselect').bind("change", function(e) {
@@ -33,6 +38,35 @@
           var url = data.url
           var json_data = jQuery.getJSON(url, data)
           alert("File available at: " + url);
+
+          var Note = Parse.Object.extend("Note");
+
+          var User = Parse.Object.extend("User");
+          var query = new Parse.Query(User);
+          query.equalTo("email", $email);
+          console.log($email);
+
+          query.find({
+            success: function(foundUser) {
+              foundUser = foundUser[0];
+              console.log(foundUser.id);
+              var note = new Note();
+              note.set("title", title);
+              note.set("content", text);
+              note.set("user", foundUser);
+
+              note.save(null, {
+                success: function(note){
+                  alert("sucess");
+                },
+                error: function(note){
+                  alert("fail");
+                }
+              });
+        }
+    });
+
+
           var itduzzit = "https://www.itduzzit.com/zherbst/api/addFile.json?token=6amr9lph4b5za8j&url=" + url
           console.log(itduzzit)
 
@@ -51,7 +85,6 @@
 
 
 })(this, this.document);
-
 
 
 
