d12108513ce3031dbb15640a3a34a060837a5cf5
Vikram Sreedhar <vikramsreedhar@Vikrams-MacBook-Pro-3.local>
2015-06-27 23:27:16 -0700

    hopefullyFinal

--- js/script.js
+++ js/script.js
@@ -1,72 +1,88 @@
 (function(window, document, undefined) {
 
-  var Parse = require('request');
+  Parse.initialize("PsO5rkxMEaRvzzig7IKiQgfpQVRNdQFpPYO9Ipg0", "y5TnJxnaphDQ51Ezzq5mXd8nCqRQ3MQdTUxIMLRm");   
+   
 
-  Parse.initialize('PsO5rkxMEaRvzzig7IKiQgfpQVRNdQFpPYO9Ipg0', '0vSsy7rKFfJtuPIwmO8gsR9L2TjdLN3LTjVZPWCc');
+ var usernameContainer = $("#user").val();
+ var title = $("#title").val();
+ var text = $("#text").val();
+ var email = $("#email").val();
+ email = "tylerweitzman@gmail.com";
 
-  var username = $("#user").val();
-  var fileTitle = $("#title").val();
-  var fileText = $("#text").val();
+   // Set an event listener on the Choose File field.
+   $('#fileselect').bind("change", function(e) {
+     var files = e.target.files || e.dataTransfer.files;
+     // Our file var now holds the selected file
+     file = files[0];
+   });
 
-    // Set an event listener on the Choose File field.
-    $('#fileselect').bind("change", function(e) {
-      var files = e.target.files || e.dataTransfer.files;
-      // Our file var now holds the selected file
-      file = files[0];
-    });
+   $('#pdf-button').click(function() {
+    var usernameContainer = $("#user").val();
+ var title = $("#title").val();
+ var text = $("#text").val();
+ var email = $("#email").val();
+     if(text !== "" && usernameContainer !== "" && title !== "" && email !== "") {
+     console.log(text);
+       } else {
+         alert("Please fill in all fields");
+       }
+   });
 
-    $('#pdf-button').click(function() {
-      if(fileText !== "" && username !== "" && fileText !== "") {
-      var itduzzit = "https://www.itduzzit.com/zherbst/api/generate.json?token=6amr9lph4b5za8j&text_file=" + username + "_" + fileTitle +".txt&contents=" + fileText;
-      var xmlhttp = new XMLHttpRequest();
-          var response = xmlhttp.open("POST", itduzzit)
-          xmlhttp.send(JSON.stringify(response));
-        } else {
-          alert("Please fill in all fields");
-        }
-    });
+   // This function is called when the user clicks on Upload to Parse. It will create the REST API request to upload this image to Parse.
+   $('#Uploadbutton').click(function() {
 
-    // This function is called when the user clicks on Upload to Parse. It will create the REST API request to upload this image to Parse.
-    $('#Uploadbutton').click(function() {
+     var serverUrl = 'https://api.parse.com/1/files/' + file.name;
 
-      var serverUrl = 'https://api.parse.com/1/files/' + file.name;
+     $.ajax({
+       type: "POST",
+       beforeSend: function(request) {
 
-      $.ajax({
-        type: "POST",
-        beforeSend: function(request) {
+         request.setRequestHeader("X-Parse-Application-Id", 'PsO5rkxMEaRvzzig7IKiQgfpQVRNdQFpPYO9Ipg0');
+         request.setRequestHeader("X-Parse-REST-API-Key", 'sy6i4A35OudquyONjSeGkkXNPOtAyUK0YEpuEgeD');
+         request.setRequestHeader("Content-Type", file.type);
+       },
+       url: serverUrl,
+       data: file,
+       processData: false,
+       contentType: false,
+       dataType: 'json',
+       success: function(data) {
+         var url = data.url
+         var json_data = jQuery.getJSON(url, data)
+         var Note = Parse.Object.extend("Note");
 
-          request.setRequestHeader("X-Parse-Application-Id", 'PsO5rkxMEaRvzzig7IKiQgfpQVRNdQFpPYO9Ipg0');
-          request.setRequestHeader("X-Parse-REST-API-Key", 'sy6i4A35OudquyONjSeGkkXNPOtAyUK0YEpuEgeD');
-          request.setRequestHeader("Content-Type", file.type);
-        },
-        url: serverUrl,
-        data: file,
-        processData: false,
-        contentType: false,
-        dataType: 'json',
-        success: function(data) {
-          var url = data.url
-          var json_data = jQuery.getJSON(url, data)
-          alert("File available at: " + url);
-          var itduzzit = "https://www.itduzzit.com/zherbst/api/addFile.json?token=6amr9lph4b5za8j&url=" + url
-          console.log(itduzzit)
+         var User = Parse.Object.extend("User");
+         var query = new Parse.Query(User);
+         query.equalTo("email", email);
+         console.log(email);
+         console.log(email);
 
-          var xmlhttp = new XMLHttpRequest();
-          var response = xmlhttp.open("POST", itduzzit)
-          xmlhttp.send(JSON.stringify(response));
-          console.log(response)
-        },
-        error: function(data) {
-          var obj = jQuery.parseJSON(data);
-          alert(obj.error);
-        }
-      });
-    });
+         query.find({
+           success: function(foundUser) {
+             foundUser = foundUser[0];
+             console.log(foundUser.id);
+             var note = new Note();
+             note.set("title", title);
+             note.set("url", url);
+             note.set("user", foundUser);
 
+             note.save(null, {
+               success: function(note){
+                 alert("sucess");
+               },
+               error: function(note){
+                 alert("fail");
+               }
+             });
+       }
+   });
+       },
+       error: function(data) {
+         var obj = jQuery.parseJSON(data);
+         alert(obj.error);
+       }
+     });
+   });
 
 
 })(this, this.document);
-
-
-
-
